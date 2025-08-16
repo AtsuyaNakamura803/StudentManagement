9f58496185fde928afaf8257a0be94fcfb63e98d
Atsuya Nakamura <asuya@gmail.com>
2025-08-16 18:43:48 +0900

    Add login feature

--- src/main/java/raisetech/Student/Management/controller/StudentController.java
+++ src/main/java/raisetech/Student/Management/controller/StudentController.java
@@ -1,40 +1,42 @@
 package raisetech.Student.Management.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.beans.factory.annotation.Autowired;
-import raisetech.Student.Management.controller.converter.StudentConverter;
-import raisetech.Student.Management.data.Student;
-import raisetech.Student.Management.data.StudentsCourses;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import raisetech.Student.Management.domain.StudentDetail;
 import raisetech.Student.Management.service.StudentService;
 
 import java.util.List;
 
-
 @Controller
 public class StudentController {
 
-    private StudentService service;
-    private StudentConverter converter;
+    private final StudentService studentService;
 
     @Autowired
-    public StudentController(StudentService service, StudentConverter converter) {
-        this.service = service;
-        this.converter = converter;
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
     }
 
     @GetMapping("/studentList")
-    public String getStudentList(Model model) {
-        List<Student> students = service.searchStudentList();
-        List<StudentsCourses> studentsCourses = service.searchStudentsCourseList();
-
-        model.addAttribute("studentList", converter.convertStudentDetails(students, studentsCourses));
+    public String showStudentList(Model model) {
+        List<StudentDetail> studentList = studentService.findAllStudentDetails();
+        model.addAttribute("studentList", studentList);
         return "studentList";
     }
 
-    @GetMapping("/studentsCourseList")
-    public List<StudentsCourses> getStudentsCourseList() {
-        return service.searchStudentsCourseList();
+    @GetMapping("/newStudent")
+    public String showForm(Model model) {
+        model.addAttribute("studentDetail", new StudentDetail());
+        return "registerStudent";
+    }
+
+    @PostMapping("/registerStudent")
+    public String registerStudent(@ModelAttribute StudentDetail studentDetail) {
+        studentService.saveStudentDetail(studentDetail);
+        return "redirect:/studentList";
     }
 }
--- src/main/java/raisetech/Student/Management/data/Student.java
+++ src/main/java/raisetech/Student/Management/data/Student.java
@@ -3,18 +3,66 @@ package raisetech.Student.Management.data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 @Setter
 public class Student {
 
-    private String id;
+    private Integer id;
     private String name;
     private String kanaName;
     private String nickname;
     private String email;
     private String area;
-    private int age;
+    private Integer age;
     private String sex;
     private String remark;
-    private boolean isDeleted;
+
+    private boolean deleted;
+
+    private List<String> courseNames = new ArrayList<>();
+
+    public void setCourseNamesFromString(String courseNamesStr) {
+        if (courseNamesStr != null && !courseNamesStr.trim().isEmpty()) {
+            this.courseNames = Arrays.stream(courseNamesStr.split(","))
+                    .map(String::trim)
+                    .filter(s -> !s.isEmpty())
+                    .collect(Collectors.toList());
+        } else {
+            this.courseNames = new ArrayList<>();
+        }
+    }
+
+    public String getCourseNamesAsString() {
+        return String.join(",", this.courseNames);
+    }
+
+    public void markDeleted() {
+        this.deleted = true;
+    }
+
+    public boolean isDeleted() {
+        return this.deleted;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", kanaName='" + kanaName + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", email='" + email + '\'' +
+                ", area='" + area + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", remark='" + remark + '\'' +
+                ", deleted=" + deleted +
+                ", courseNames=" + courseNames +
+                '}';
+    }
 }
